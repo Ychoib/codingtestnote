617407bc2b77fb878cb7fe5687e5f959ad4454f7
Ychoib <51788100+Ychoib@users.noreply.github.com>
2021-08-03 15:53:49 +0900

    21-08-03

---
+++ 14501_퇴사(dp문제 다시 풀기) .py
@@ -0,0 +1,14 @@
+n = int(input())
+import sys
+schedule = [ list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+
+dp = [0 for i in range(n+1)]
+
+for i in range(n-1,-1,-1):
+    if i + schedule[i][0] > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1],schedule[i][1] + dp[i+schedule[i][0]])
+print(max(dp))
+
+
